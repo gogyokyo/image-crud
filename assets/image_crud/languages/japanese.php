bfa72fbe2e5aad3aa62f6949ebe5a8d98ed57a31
gogyokyo <go.gyokyo@wisematrix.co.jp>
2016-05-19 13:34:08 +0900

    Create japanese.php
    japanese translation

---
+++ assets/image_crud/languages/japanese.php
@@ -0,0 +1,16 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$lang['upload_button'] = 'ここでファイルをアップロード';
+$lang['upload-drop-area'] = 'ここでファイルをドロップ後アップロード';
+$lang['upload-cancel'] = 'キャンセル';
+$lang['upload-failed'] = '失敗しました';
+
+$lang['loading'] = 'ローディング, 少し待って下さい...';
+$lang['deleting'] = '削除中, 少し待って下さい...';
+$lang['saving_title'] = 'セーブタイトル中...';
+
+$lang['list_delete'] = '削除';
+$lang['alert_delete'] = '本当にこの画像を削除しますか?';
+
+/* End of file english.php */
+/* Location: ./assets/image_crud/languages/japanese.php */
