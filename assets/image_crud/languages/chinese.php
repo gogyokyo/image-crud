64d6d4365fb4363031e27cf0dfe5b45e21b391f9
gogyokyo <go.gyokyo@wisematrix.co.jp>
2016-05-19 13:33:30 +0900

    Create chinese.php
    chinese translation

---
+++ assets/image_crud/languages/chinese.php
@@ -0,0 +1,16 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$lang['upload_button'] = '在这里上传文件';
+$lang['upload-drop-area'] = '拖动文件到这里上传';
+$lang['upload-cancel'] = '取消上传';
+$lang['upload-failed'] = '上传失败';
+
+$lang['loading'] = '加载中,请稍后...';
+$lang['deleting'] = '删除中,请稍后...';
+$lang['saving_title'] = '保存标题中...';
+
+$lang['list_delete'] = '删除';
+$lang['alert_delete'] = '删除后无法恢复,确定删除?';
+
+/* End of file english.php */
+/* Location: ./assets/image_crud/languages/chinese.php */
